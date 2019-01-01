6245481aa41a3dcf0beea29afc0f069badcc0e46
Mattias <mattias.erik.karlsson@gmail.com>
2019-01-01 20:32:03 +0100

    Modified size for Neogeo CD

--- main.js
+++ main.js
@@ -3,47 +3,45 @@ $(function(){
   var lastUrl = null;
   var $mainForm = $("form#mainForm");
 
+  var systemDimensions = {
+    "snes": { 
+      sw: 3366,
+      sh: 2100,
+      dx: (rotate) => { return rotate ? -3366 : -1832 },
+      dy: 0,
+      dw: (rotate) => { return rotate ? 2100 : 1534 },
+      dh: (rotate) => { return rotate ? 1534 : 2100 }
+     },
+    "neogeocd": {
+      sw: 3225,
+      sh: 2156,
+      dx: (rotate) => { return rotate ? -3225 : -1687 },
+      dy: 0,
+      dw: (rotate) => { return rotate ? 2154 : 1538 },
+      dh: (rotate) => { return rotate ? 1538 : 2154 }
+     },
+     "genesismegadrive": {
+      sw: 3366,
+      sh: 2100,
+      dx: (rotate) => { return rotate ? -3366 : -1835 },
+      dy: 0,
+      dw: (rotate) => { return rotate ? 2156 : 1520 },
+      dh: (rotate) => { return rotate ? 1520 : 2156 }
+     }
+  }
+
   function processImage(img){
-
-    var dim = {
-      "snes": { 
-        sw: 3366,
-        sh: 2100,
-        dx: () => { return rotate ? -3366 : -1832 },
-        dy: 0,
-        dw: () => { return rotate ? 2100 : 1534 },
-        dh: () => { return rotate ? 1534 : 2100 }
-       },
-      "neogeocd": {
-        sw: 3225,
-        sh: 2156,
-        dx: () => { return rotate ? -3225 : -1520 },
-        dy: 0,
-        dw: () => { return rotate ? 2156 : 1705 },
-        dh: () => { return rotate ? 1705 : 2156 }
-       },
-       "genesismegadrive": {
-        sw: 3366,
-        sh: 2100,
-        dx: () => { return rotate ? -3366 : -1835 },
-        dy: 0,
-        dw: () => { return rotate ? 2156 : 1520 },
-        dh: () => { return rotate ? 1520 : 2156 }
-       }
-    }
-
-    var system = dim[$("#system").val()];
-
     var rotate = $("#rotate").prop("checked")
+    var system = systemDimensions[$("#system").val()];
     var c = document.createElement('canvas');
     c.id = "mainCanvas";
-    c.width = system.dw();
-    c.height = system.dh();
+    c.width = system.dw(rotate);
+    c.height = system.dh(rotate);
     var ctx = c.getContext('2d');
     if(rotate){
       ctx.save();
       ctx.rotate(-90*Math.PI/180);
-      ctx.drawImage(img, system.dx(), system.dy, system.sw, system.sh);
+      ctx.drawImage(img, system.dx(rotate), system.dy, system.sw, system.sh);
       ctx.restore();
     } else {
       ctx.drawImage(img, system.dx(), system.dy, system.sw, system.sh);
